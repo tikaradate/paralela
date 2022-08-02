2702f0879b0573cde7c65ce423445843a4f2982d
Vinicius Tikara <vtvd19@inf.ufpr.br>
2022-08-02 11:58:58 -0300

    Cria csv correto

--- processdata.py
+++ processdata.py
@@ -1,5 +1,7 @@
 import glob
 import statistics
+import pandas as pd
+import matplotlib.pyplot as plt
 
 dirs = []
 for i in range(1, 51):
@@ -17,5 +19,19 @@ for _dir in dirs:
             files[name] = [time] 
 
 dict(sorted(files.items()))
+table = {}
 for f in dict(sorted(files.items())):
-    print("%s avg: %f s stdev: %f s" % (f, statistics.mean(files[f]), statistics.stdev(files[f])))
+    # print("%s avg: %f s stdev: %f s" % (f, statistics.mean(files[f]), statistics.stdev(files[f])))
+    mean = statistics.mean(files[f])
+    stdev = statistics.stdev(files[f])
+    if(f in table.keys()):
+        table[f].append(mean)
+        table[f].append(stdev)
+    else:
+        table[f] = [mean]
+        table[f].append(stdev)
+
+df = pd.DataFrame(table, index = ["Média", "Desvio Padrão"])
+df = df.transpose()
+print(df)
+df.to_csv("results.csv")
