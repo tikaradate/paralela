192c613ffb6e7dc069fde4f5f0bcfa4caf97ff5e
tikaradate <tikaradate@gmail.com>
2022-08-02 21:14:25 -0300

    Testes estranhos~

--- runextra.sh
+++ runextra.sh
@@ -5,11 +5,12 @@ g++ -fopenmp parallel.cpp -O3 -o paralelo
 STARTALL=$(date +%s.%N)
 for (( i = 1; i <= 50; i++))
 do
+    mkdir exp"${i}"
     echo "-------------- Starting test number ${i} --------------"
     for j in {5000..65000..5000}
     do
         echo $DIFF > exp"${i}"/seq_${k}_${j}.time
-        for k in 4 5
+        for k in 7 8 10 11 13
         do  
             export OMP_NUM_THREADS=${k}
             echo "Executing parallel with size ${j} and with ${k} threads"
